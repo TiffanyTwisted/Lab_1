bb470f08ff32db0a4130a899aeb66af5ec26658f
Polina <rct.shirkoPA@bsu.by>
2019-12-27 18:46:28 +0300

    feat: init lab01

---
+++ src/bsu/rfe/java/lab1/Food.java
@@ -0,0 +1,14 @@
+package bsu.rfe.java.lab1;
+
+public abstract class Food implements INutritious, IConsumable{
+    int calories = 0;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public Food(String name) {
+        this.name = name;
+    }
+
+}
